b474d78ea4f46bfc6778bbb2b6441380ca1edbe2
nerd <peter0x000@gmail.com>
2024-06-12 22:01:09 -0400

    orca\!

--- homes/nerd/packages/gui/3dprint.nix
+++ homes/nerd/packages/gui/3dprint.nix
@@ -1,5 +1,17 @@
-_: {
-  config = {
+{
+  osConfig,
+  pkgs,
+  lib,
+  self',
+  ...
+}: let
+  inherit (lib) mkIf;
+  inherit (osConfig) modules;
+
+  sys = modules.system;
+  prg = sys.programs;
+in {
+  config = mkIf (prg.gui.enable && sys.printing."3d".enable) {
     home.packages = with pkgs; [
       self'.packages.orca-slicer
       prusa-slicer
